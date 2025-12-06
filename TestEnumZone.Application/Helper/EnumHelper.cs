266eac84dfc0f4f2b93c34ab9bd037149466660c
GS-755 <tringuyenminh2003@gmail.com>
2025-12-06 13:09:24 +0700

    Update: Route stderr, add global constant + XML comments for `EnumHelper.cs`

--- TestEnumZone.Application/Helper/EnumHelper.cs
+++ TestEnumZone.Application/Helper/EnumHelper.cs
@@ -6,11 +6,16 @@ namespace TestEnumZone.Application.Helper
 {
     public static class EnumHelper
     {
+        /// <summary>
+        /// Function to get <c>Enum description</c> with datatype: <c>string</c>
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns>Enum description string</returns>
         public static string GetEnumDescription(Enum value)
         {
             if (value == null)
             {
-                Console.WriteLine("EnumHelper.GetEnumDescription Invalid params!");
+                Console.Error.WriteLine("EnumHelper.GetEnumDescription Invalid params!");
                 return string.Empty;
             }
             FieldInfo fi = value.GetType().GetField(value.ToString());
@@ -25,33 +30,56 @@ namespace TestEnumZone.Application.Helper
 
             return value.ToString();
         }
+        /// <summary>
+        /// Function to get <c>Enum name</c> with datatype: <c>string</c>
+        /// </summary>
+        /// <typeparam name="TEnum"></typeparam>
+        /// <param name="value"></param>
+        /// <returns>Enum name string</returns>
         public static string GetEnumString<TEnum>(TEnum? value)
             where TEnum : struct, Enum
         {
             if (value == null)
             {
-                Console.WriteLine("EnumHelper.GetEnumValueString Invalid params!");
+                Console.Error.WriteLine("EnumHelper.GetEnumValueString Invalid params!");
                 return string.Empty;
             }
 
-            return value.ToString();
+            return value.ToString() ?? string.Empty;
         }
-
+        /// <summary>
+        /// Function to get <c>Enum value</c> with datatype: <c>int</c>
+        /// <para></para>
+        /// As current enum switch convention is Positive integer, so I think <c>Negative integer return</c> should be resonable for exceptional case.
+        /// </summary>
+        /// <typeparam name="TEnum"></typeparam>
+        /// <param name="value"></param>
+        /// <returns>Enum value parsed to int</returns>
         public static int GetEnumValueInt<TEnum>(TEnum? value)
             where TEnum : struct, Enum
         {
             if (value == null)
             {
                 Console.WriteLine("EnumHelper.GetEnumValueInt Invalid params!");
-                return -1; // Return Negative value for invalid case 
+                return GlobalConstants.ENUM_FATAL; // Return Negative value for invalid case 
             }
-            int raw = Convert.ToInt32(value.Value);
-            if (raw < int.MinValue || raw > int.MaxValue)
+            try
             {
-                Console.WriteLine("GetEnumDescription.GetEnumInt Passed enum exceeds int range! - Enum: {0}, value {1} ", value, value.Value);
-            }
+                int raw = Convert.ToInt32(value.Value);
+                if (raw < int.MinValue || raw > int.MaxValue)
+                {
+                    Console.Error.WriteLine("GetEnumDescription.GetEnumInt Passed enum exceeds int range! - Enum: {0}, value {1} ", value, value.Value);
+                }
 
-            return (int)raw;
+                return raw;
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine("GetEnumDescription.GetEnumInt Unhandled exception: - Enum: {0}", value);
+                Console.Error.WriteLine(ex);
+
+                return GlobalConstants.ENUM_FATAL;
+            }
         }
     }
 }
